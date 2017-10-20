cae69dc89210976beebfbdbf08a5e69b1ded63d7
陈嘉涵 <chenjiahan@youzan.com>
2017-10-20 19:38:57 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# vant-demo
+
+> Collection of vant demos.
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <van-cell-group class="item-list">
+      <van-cell isLink icon="points" title="我的积分" />
+      <van-cell isLink icon="gift" title="我收到的礼物" />
+    </van-cell-group>
+  </div>
+</template>
+
+<script>
+import { Cell, CellGroup, Icon } from 'vant';
+
+export default {
+  components: {
+    [Cell.name]: Cell,
+    [CellGroup.name]: CellGroup,
+    [Icon.name]: Icon
+  }
+};
+</script>
+
+<style>
+body {
+  background-color: #f8f8f8;
+}
+
+.van-cell__title .van-icon {
+  font-size: 18px;
+}
+</style>
