34100f61bf2ef2a14d2549e8d09282a94eea021b
陈嘉涵 <chenjiahan@youzan.com>
2017-10-24 11:13:23 +0800

    build

--- dist/js/vendor.f0335f126a95cb43c09d.js
+++ dist/js/vendor.f0335f126a95cb43c09d.js
@@ -1,4 +1,4 @@
-webpackJsonp([1],{"/5sW":function(t,e,n){"use strict";(function(t,n){/*!
+webpackJsonp([2],{"/5sW":function(t,e,n){"use strict";(function(t,n){/*!
  * Vue.js v2.5.2
  * (c) 2014-2017 Evan You
  * Released under the MIT License.
