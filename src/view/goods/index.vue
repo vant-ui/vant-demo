c2ce54df48248a89638d4ae65940ee25af4f1348
陈嘉涵 <chenjiahan@youzan.com>
2018-03-19 17:48:51 +0800

    feat: 商品跳转购物车

--- src/view/goods/index.vue
+++ src/view/goods/index.vue
@@ -18,30 +18,30 @@
     </van-cell-group>
 
     <van-cell-group class="goods-cell-group">
-      <van-cell value="进入店铺" icon="shop" is-link>
+      <van-cell value="进入店铺" icon="shop" is-link @click="sorry">
         <template slot="title">
           <span class="van-cell-text">有赞的店</span>
           <van-tag type="danger">官方</van-tag>
         </template>
       </van-cell>
-      <van-cell title="线下门店" icon="location" is-link />
+      <van-cell title="线下门店" icon="location" is-link @click="sorry" />
     </van-cell-group>
 
     <van-cell-group class="goods-cell-group">
-      <van-cell title="查看商品详情" is-link />
+      <van-cell title="查看商品详情" is-link @click="sorry" />
     </van-cell-group>
 
     <van-goods-action>
-      <van-goods-action-mini-btn icon="chat">
+      <van-goods-action-mini-btn icon="chat" @click="sorry">
         客服
       </van-goods-action-mini-btn>
-      <van-goods-action-mini-btn icon="cart">
+      <van-goods-action-mini-btn icon="cart" @click="onClickCart">
         购物车
       </van-goods-action-mini-btn>
-      <van-goods-action-big-btn>
+      <van-goods-action-big-btn @click="sorry">
         加入购物车
       </van-goods-action-big-btn>
-      <van-goods-action-big-btn primary>
+      <van-goods-action-big-btn primary @click="sorry">
         立即购买
       </van-goods-action-big-btn>
     </van-goods-action>
@@ -56,6 +56,7 @@ import {
   Cell,
   CellGroup,
   Swipe,
+  Toast,
   SwipeItem,
   GoodsAction,
   GoodsActionBigBtn,
@@ -94,6 +95,14 @@ export default {
   methods: {
     formatPrice() {
       return '¥' + (this.goods.price / 100).toFixed(2);
+    },
+
+    onClickCart() {
+      this.$router.push('cart');
+    },
+
+    sorry() {
+      Toast('暂无后续逻辑~');
     }
   }
 };
