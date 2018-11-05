da05b08328db405f376c60f64827f38b3ea2deb6
陈嘉涵 <chenjiahan@youzan.com>
2018-11-05 09:35:20 +0800

    remove duplicate code

--- rem/src/main.js
+++ rem/src/main.js
@@ -1,10 +1,2 @@
 import 'amfe-flexible';
-import Vue from 'vue';
-import App from './App';
-import { router } from './router';
-
-new Vue({
-  router,
-  el: '#app',
-  render: h => h(App)
-});
+import '../../base/src/main';
