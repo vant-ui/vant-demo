6d3e2935c0f6dced960c7c635f71d36d94cb9cc7
陈嘉涵 <chenjiahan@youzan.com>
2019-04-25 11:16:38 +0800

    [improvement] add support for circle rem

--- rem/vue.config.js
+++ rem/vue.config.js
@@ -11,7 +11,10 @@ module.exports = {
           autoprefixer(),
           pxtorem({
             rootValue: 37.5,
-            propList: ['*']
+            propList: ['*'],
+            // 该项仅在使用 Circle 组件时需要
+            // 原因参见 https://github.com/youzan/vant/issues/1948
+            selectorBlackList: ['van-circle__layer']
           })
         ]
       }
