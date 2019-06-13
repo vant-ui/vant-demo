146d5a5600edc68abd239c1beb27f8a68d8c2315
陈嘉涵 <chenjiahan@youzan.com>
2019-06-13 16:51:34 +0800

    add nuxt demo

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <van-button>按钮</van-button>
+  </div>
+</template>
+
+<script>
+import '../components'
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
