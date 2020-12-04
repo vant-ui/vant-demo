b4c5eb0b134a0ecf8824d19d920cb4be58465885
chenjiahan <chenjiahan@youzan.com>
2020-12-04 11:39:43 +0800

    chore: rename vue2 folder

--- vant/base/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-import App from './App';
-import { router } from './router';
-
-new Vue({
-  router,
-  el: '#app',
-  render: h => h(App)
-});
--- vant/base/vue.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  outputDir: 'dist',
-  publicPath: process.env.NODE_ENV === 'production' ? '/vant-demo/' : '/'
-};
