6bb11435bd88f124c022bf17e2c85c451b2d9551
chenjiahan <chenjiahan.jait@bytedance.com>
2022-06-11 19:51:28 +0800

    feat: update vite demo, using unplugin-vue-components

--- vant/vite/vite.config.js
+++ vant/vite/vite.config.js
@@ -1,17 +1,12 @@
 import vue from "@vitejs/plugin-vue";
-import styleImport from "vite-plugin-style-import";
+import Components from "unplugin-vue-components/vite";
+import { VantResolver } from "unplugin-vue-components/resolvers";
 
 export default {
   plugins: [
     vue(),
-    styleImport({
-      libs: [
-        {
-          libraryName: "vant",
-          esModule: true,
-          resolveStyle: (name) => `vant/es/${name}/style`,
-        },
-      ],
+    Components({
+      resolvers: [VantResolver()],
     }),
   ],
 };
