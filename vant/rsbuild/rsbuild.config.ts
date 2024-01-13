f99a34a9cbe5f838d69338513a36b7fce8e39b6d
chenjiahan <chenjiahan.jait@bytedance.com>
2024-01-13 11:51:51 +0800

    feat: improve Rsbuild demo

--- vant/rsbuild/rsbuild.config.ts
+++ vant/rsbuild/rsbuild.config.ts
@@ -1,6 +1,17 @@
 import { defineConfig } from "@rsbuild/core";
 import { pluginVue } from "@rsbuild/plugin-vue";
+import Components from "unplugin-vue-components/rspack";
+import { VantResolver } from "@vant/auto-import-resolver";
 
 export default defineConfig({
   plugins: [pluginVue()],
+  tools: {
+    rspack: {
+      plugins: [
+        Components({
+          resolvers: [VantResolver()],
+        }),
+      ],
+    },
+  },
 });
--- vant/rsbuild/src/index.ts
+++ vant/rsbuild/src/index.ts
@@ -1,6 +1,5 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import 'vant/lib/index.css';
 
 const app = createApp(App);
 app.mount('#root');
